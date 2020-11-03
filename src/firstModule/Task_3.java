333403cb272d48e60f700b088b5bfd394d954dfa
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-04 00:52:31 +0300

    ``````3.11.2020

--- src/firstModule/Task_3.java
+++ src/firstModule/Task_3.java
@@ -1,4 +1,15 @@
 package firstModule;
 
+import static java.lang.Math.*;
+
 public class Task_3 {
+
+    private static final double X = Math.random() * 360;
+    private static final double Y = Math.random() * 360;
+
+    public static void main(String[] args) {
+
+        double result = (sin(X) + cos(Y)) / (cos(X) - sin(Y)) * tan(X);
+        System.out.println("function calculation result" + result);
+    }
 }
