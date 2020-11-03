ef49e8ef1a14785d28713116d76c8c8f51c02104
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-04 01:05:59 +0300

    ``````3.11.2020

--- src/firstModule/Task_3.java
+++ src/firstModule/Task_3.java
@@ -4,12 +4,12 @@ import static java.lang.Math.*;
 
 public class Task_3 {
 
-    private static final double X = Math.random() * 360;
-    private static final double Y = Math.random() * 360;
+    private static final double X_TO_RADIANS = Math.toRadians(random() * 360);
+    private static final double Y_TO_RADIANS = Math.toRadians(random() * 360);
 
     public static void main(String[] args) {
 
-        double result = (sin(X) + cos(Y)) / (cos(X) - sin(Y)) * tan(X);
-        System.out.println("function calculation result" + result);
+        double result = (sin(X_TO_RADIANS) + cos(Y_TO_RADIANS)) / (cos(X_TO_RADIANS) - sin(Y_TO_RADIANS)) * tan(X_TO_RADIANS*Y_TO_RADIANS);
+        System.out.println("function calculation result --> " + result);
     }
 }
