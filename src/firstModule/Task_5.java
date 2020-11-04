c60655afb9524a2777562f8da31dc32bfd189575
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-04 21:46:16 +0300

    ``````3.11.2020

--- src/firstModule/Task_5.java
+++ src/firstModule/Task_5.java
@@ -1,4 +1,16 @@
 package firstModule;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Task_5 {
+    public static void main(String[] args) {
+
+        String T = JOptionPane.showInputDialog("Enter amount of seconds ");
+        int amountSec = Integer.parseInt(T);
+        int nHours = (amountSec%86400)/3600;
+        int nMin = ((amountSec%86400)%3600) /60;
+        int nSec =(((amountSec%86400)%3600)%60);
+        JOptionPane.showMessageDialog(null,"That is "+nHours+"  hour(s), "+nMin+"  minute(s), and "+nSec+" second(s). ");
+    }
 }
