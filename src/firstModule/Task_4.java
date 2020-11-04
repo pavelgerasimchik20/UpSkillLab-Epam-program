43cc1b6a3c39c4faf53888a6c2ba795ec7d8aa0e
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-04 19:35:59 +0300

    ``````3.11.2020

--- src/firstModule/Task_4.java
+++ src/firstModule/Task_4.java
@@ -1,4 +1,18 @@
 package firstModule;
 
+import java.util.Scanner;
+
 public class Task_4 {
+    public static void main(String[] args) {
+
+        double inputVal = 1, swapVal;
+        while (inputVal != 0) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("ENTER DOUBLE VALUE in format XXX.YYY ");
+            inputVal = scanner.nextDouble();
+            swapVal = (inputVal * 1000) % 1000 + (int) inputVal / 1000.0;
+            System.out.println(swapVal);
+            System.out.println(" enter 0 to exit ");
+        }
+    }
 }
