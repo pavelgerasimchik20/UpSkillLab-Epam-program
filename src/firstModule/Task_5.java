5cd95f089ee74983387677977acf979a72a339b5
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-04 23:54:04 +0300

    4.11.2020

--- src/firstModule/Task_5.java
+++ src/firstModule/Task_5.java
@@ -8,9 +8,9 @@ public class Task_5 {
 
         String T = JOptionPane.showInputDialog("Enter amount of seconds ");
         int amountSec = Integer.parseInt(T);
-        int hours = (amountSec%86400)/3600;
-        int minutes = ((amountSec%86400)%3600) /60;
-        int secs =(((amountSec%86400)%3600)%60);
-        JOptionPane.showMessageDialog(null, String.format(Locale.getDefault(), "%02dч:%02dмин:%02dс", hours, minutes, secs));
+        int hours = amountSec/3600;
+        int minutes = (amountSec%3600) /60;
+        int secs =((amountSec%3600)%60);
+        JOptionPane.showMessageDialog(null, String.format( "%02dч:%02dмин:%02dс", hours, minutes, secs));
     }
 }
