0ad010250900b6e19564cc46223ecdfd622f830e
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-08 16:51:31 +0300

    8.11.2020

--- src/firstModule/cycles/Task_3.java
+++ src/firstModule/cycles/Task_3.java
@@ -1,4 +1,14 @@
 package firstModule.cycles;
 
-public class Task_3 {
+public class Task_3 { // Найти сумму квадратов первых ста чисел.
+    public static void main(String[] args) {
+
+        long sum = 0;
+        int runDigit = 1;
+        int finalDigit = 100;
+        for (; runDigit <= finalDigit; runDigit++) {
+            sum += runDigit * runDigit;
+        }
+        System.out.println("Сумма квадратов первых ста чисел: " + sum);
+    }
 }
