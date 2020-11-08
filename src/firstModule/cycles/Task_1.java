1c604547c45f93bdb81439336a43988049b63850
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-08 16:44:25 +0300

    8.11.2020

--- src/firstModule/cycles/Task_1.java
+++ src/firstModule/cycles/Task_1.java
@@ -1,12 +1,23 @@
 package firstModule.cycles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Task_1 {
+public class Task_1 { // Напишите программу, где пользователь вводит любое целое положительное число.
+    // А программа суммирует все числа от 1 до введенного пользователем числа.
     public static void main(String[] args) {
 
+        int sum = 0;
         System.out.println("Введите любое целое положительное число: ");
         Scanner scanner = new Scanner(System.in);
-        int natDigit = scanner.nextInt();
+        try {
+            int inputDigit = scanner.nextInt();
+            for (int startDigit = 1; startDigit < inputDigit; startDigit++) {
+                sum += startDigit;
+            }
+            System.out.println("Результат: " + sum);
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Необходимо ввести целое число !");
+        }
     }
 }
