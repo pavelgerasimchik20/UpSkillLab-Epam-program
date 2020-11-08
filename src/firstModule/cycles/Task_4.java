d7a1a7c44f8aa3937050738207f5a7b3f7fd7b43
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-08 17:47:48 +0300

    8.11.2020

--- src/firstModule/cycles/Task_4.java
+++ src/firstModule/cycles/Task_4.java
@@ -1,4 +1,27 @@
 package firstModule.cycles;
 
-public class Task_4 {
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Task_4 { // Составить программу нахождения произведения квадратов первых двухсот чисел
+
+    public static void main(String[] args) {
+
+            int num = 200;
+            System.out.println("Произведение квадратов первых "+ num + " чисел = "+ Factorial.factorial(num));
+
+    }
 }
+class Factorial {
+
+    static BigInteger factorial(int num) {
+        BigInteger bigInteger = BigInteger.ONE;
+        int runDigit = 1;
+        for (; runDigit <= num; runDigit++) {
+            bigInteger = bigInteger.multiply(BigInteger.valueOf(runDigit*runDigit));
+        }
+        return bigInteger;
+
+    }
+}
+
