7453e94cba177fcb4d61b9081d7f1acbb7d6be58
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-10 18:05:42 +0300

    10.11.2020

--- src/firstModule/branching/Task_5.java
+++ src/firstModule/branching/Task_5.java
@@ -1,12 +1,19 @@
 package firstModule.branching;
 
-import static java.lang.Math.pow;
+import java.util.Scanner;
 
 public class Task_5 {
 
     public static void main(String[] args) {
 
-
-
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y;
+        if (x <= 3) {
+            y = (Math.pow(x, 2) - 3 * x + 9);
+        } else {
+            y =( 1 / (Math.pow(x, 3) + 6) );
+        }
+        System.out.println("y= " + y);
     }
 }
--- src/firstModule/cycles/Task_2.java
+++ src/firstModule/cycles/Task_2.java
@@ -1,6 +1,31 @@
 package firstModule.cycles;
 
+import java.util.Scanner;
+
 public class Task_2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double a, b, x, h, y;
+
+        System.out.println(" x= ");
+        x = scanner.nextDouble();
+        System.out.println("h =");
+        h = scanner.nextDouble();
+        System.out.println("a=");
+        a = scanner.nextDouble();
+        System.out.println("b=");
+        b = scanner.nextDouble();
+
+        for (x = a; x <= b; x += h) {
+
+            if (x > 2) {
+                y = x;
+                System.out.println("y =" + y);
+            } else {
+                y = -x;
+                System.out.println("y =" + y);
+            }
+        }
     }
 }
