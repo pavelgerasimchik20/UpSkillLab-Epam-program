e0ef9745bf7f2906d5befe60e83e1b74984ff22d
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-10 05:10:28 +0300

    10.11.2020

--- src/firstModule/cycles/Task_5.java
+++ src/firstModule/cycles/Task_5.java
@@ -1,7 +1,18 @@
 package firstModule.cycles;
 
+import java.util.Scanner;
+
 public class Task_5 { //  Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ну вот тут то можно вводить ЧТО УГОДНО без обработки и проверок : ");
+        String str = scanner.nextLine();
+        char[] charArray = str.toCharArray();
+        StringBuilder temporary = new StringBuilder("");
+        for (char everyChar : charArray) {
+            temporary.append(String.format("\\u%05x ", (int) everyChar));
+        }
+        System.out.println("На языке юникода это выглядит так: " + temporary);
     }
 }
