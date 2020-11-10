b955d7b14dbdf587954011f1363136f9ed334944
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-10 04:39:21 +0300

    10.11.2020

--- src/firstModule/cycles/Task_2.java
+++ src/firstModule/cycles/Task_2.java
@@ -1,4 +1,6 @@
 package firstModule.cycles;
 
 public class Task_2 {
+    public static void main(String[] args) {
+    }
 }
--- src/firstModule/cycles/Task_7.java
+++ src/firstModule/cycles/Task_7.java
@@ -1,33 +1,60 @@
 package firstModule.cycles;
 
+import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task_7 { // Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
+
+    private static long firstNum;                      // long только для того, чтобы пользователь мог написать немного больше цифр в число  ))
+    private static long secondNum;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int firstNum = scanner.nextInt();
-        System.out.println("Enter second number: ");
-        int secondNum = scanner.nextInt();
-        numberEnter(firstNum);
-        numberEnter(secondNum);
+        try {
+            System.out.println("Enter first number: ");
+            firstNum = scanner.nextLong();
+            if (firstNum < 0) {
+                firstNum = Math.abs(firstNum);
+            }
+            System.out.println("Enter second number: ");
+            secondNum = scanner.nextLong();
+            if (secondNum < 0) {
+                secondNum = Math.abs(secondNum);
+            }
+            System.out.println("Common digits: "+compareArrays(createArray(firstNum), createArray(secondNum))); // омайгад... надеюсь это тебе понравится ))
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Enter integer number !");
+        }
     }
 
-    public static int[] numberEnter(int digit) {
-        int[] array = new int[10];
+    public static Long[] createArray(long anyNumber) {
+        Long[] array = new Long[20];
         int i = 0;
-        while (digit > 0) {
-            array[i] = digit % 10;
-            digit = digit / 10;
+        if (anyNumber == 0) {
+            array[i] = Long.valueOf(0);
+        }
+        while (anyNumber > 0) {
+            array[i] = anyNumber % 10;
+            anyNumber = anyNumber / 10;
             i++;
         }
-        for (int j = 0; j < array.length; j++) {
-            if (array[j] != 0) {
-                System.out.print(" " + array[j]);
+        return array;
+    }
+
+    public static Set<Long> compareArrays(Long[] a, Long[] b) {
+        Set<Long> set = new HashSet();
+        for (int i = 0; i < a.length; i++) {
+            Long x = a[i];
+            for (int j = 0; j < b.length; j++) {
+                if (x == b[j] && x != null) {
+                    set.add(b[j]);
+                    break;
+                }
             }
         }
-        System.out.println(" ");
-        return array;
+        return set;
     }
 }
