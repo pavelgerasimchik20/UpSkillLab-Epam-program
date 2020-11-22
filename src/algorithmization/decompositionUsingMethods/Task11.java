003d3baf2ab576a3714cc1f107a53ab8fb8d9869
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-23 00:23:45 +0300

    23/11/2020

--- src/algorithmization/decompositionUsingMethods/Task11.java
+++ src/algorithmization/decompositionUsingMethods/Task11.java
@@ -1,4 +1,36 @@
 package algorithmization.decompositionUsingMethods;
 
-public class Task11 { //
+public class Task11 { // Написать метод(методы), определяющий, в каком из данных двух чисел больше цифр.
+
+    private static int i;
+    private static int j;
+
+    public static void main(String[] args) {
+
+        int firstNum = 134533;
+        int secondNum = 1345;
+        System.out.println(whoIsMore(firstNum,secondNum));
+
+    }
+
+    public static int whoIsMore(int a, int b) {
+        int copyA = a;
+        while (a > 0) {
+            a = a / 10;
+            i++;
+        }
+        int copyB=b;
+        while (b > 0) {
+            b = b / 10;
+            j++;
+        }
+        if (i > j) {
+            return copyA;
+        } else if (j > i) {
+            return copyB;
+        } else {
+            System.out.println("количество цифр в числах ровно");
+        }
+        return -1;
+    }
 }
