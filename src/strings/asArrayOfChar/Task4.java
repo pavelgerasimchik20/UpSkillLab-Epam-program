055434e23d255bae023db2b54691f3bb396c29a9
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-11-29 19:03:00 +0300

    28/11/2020

---
+++ src/strings/asArrayOfChar/Task4.java
@@ -0,0 +1,21 @@
+package strings.asArrayOfChar;
+
+import javax.swing.*;
+
+public class Task4 { //  В строке найти количество чисел.
+    public static void main(String[] args) {
+
+        String str;
+        int count = 0;
+        str = JOptionPane.showInputDialog("Введите текст :");
+        str = str.replaceAll("[-!.,?:;+=<>'\"]", "");
+        str = str.replaceAll("\\D+", " ");
+        String trimStr = str.trim();
+        String[] txt = trimStr.split(" +");
+        for (String number: txt){
+            count++;
+        }
+        JOptionPane.showMessageDialog(null, "Количество чисел в данном тексте = " + count);
+
+    }
+}
