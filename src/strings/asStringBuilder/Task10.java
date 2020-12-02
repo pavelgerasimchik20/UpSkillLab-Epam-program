e2dc5497fb4afed07558b83415545059bb166994
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-12-02 19:44:05 +0300

    2/12/2020

---
+++ src/strings/asStringBuilder/Task10.java
@@ -0,0 +1,25 @@
+package strings.asStringBuilder;
+
+import javax.swing.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task10 { // Строка X состоит из нескольких предложений, каждое из которых кончается точкой,
+    // восклицательным или вопросительным знаком. Определить количество предложений в строке X.
+
+    public static void main(String... args) {
+
+        String str;
+        Pattern pattern = Pattern.compile("[.!?]");
+
+        int count = 0;
+
+        str = JOptionPane.showInputDialog("Введите текст :");
+        Matcher matcherLower = pattern.matcher(str);
+        while (matcherLower.find()) {
+            count++;
+        }
+
+        JOptionPane.showMessageDialog(null, "suggestions = " + count );
+    }
+}
